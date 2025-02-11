29d699cbadd0725b3c0830b48a8e5836639605f1
guptashashankmca <shashankgupta.mca@outlook.com>
2025-02-11 14:54:41 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,10 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={()=>{
+          setCount(count+1);
+        }}>Click me</button>
     </div>
   )
 }
