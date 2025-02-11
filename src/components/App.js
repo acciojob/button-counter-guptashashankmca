9172c82425b5c9fc29c371ba98bc0203a812101d
guptashashankmca <shashankgupta.mca@outlook.com>
2025-02-11 14:58:47 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
         <p>Button clicked {count} times</p>
